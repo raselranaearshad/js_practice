136bf4ee197a5799ea6038552ceb876cd4f123a8
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-11 00:35:10 +0600

    Js Operators

---
+++ 06_js_operators/operators.js
@@ -0,0 +1,29 @@
+var x = 20
+var y = 30
+// This is a example of addition operator.
+var z = x + y
+document.write(z + "<br>");
+
+let X = 20
+let Y = 10
+// This is a example of Subtraction operator.
+let Z = X - Y
+document.write(Z + "<br>");
+
+let A = 10
+let B = 10
+// This is a example of Multiplication Operator.
+let C = A * B
+document.write(C + "<br>");
+
+let d = 100
+let f = 10
+// This is a example of Division Operator.
+let e = 100 / 10
+document.write(e + "<br>")
+
+let D = 200
+let F = 10
+// This is a example of Modulus Operator.
+let E = 200 % 10
+document.writeln(E);
