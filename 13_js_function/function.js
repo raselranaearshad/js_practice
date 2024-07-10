747c189c92b4ff82b48261105e57ac9befc3fd84
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-11 02:21:59 +0600

    Js Function

---
+++ 13_js_function/function.js
@@ -0,0 +1,18 @@
+/* The advantage of using function is */
+/* 
+  1. Code Reusability
+  2. Less Code
+  3. Easy to Understand.
+*/ 
+
+function addTwoNumber (){
+  let num1 = 10
+  let num2 = 20
+  let num3 = num1 + num2
+  document.write(num3 + "<br>")
+};
+// This is a example of Js Function
+addTwoNumber();
+addTwoNumber();
+addTwoNumber();
+addTwoNumber();
