f2476cf2f53cc42d050272366d0dd136ba2e16b6
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-11 02:39:00 +0600

    Js Function Parametar

---
+++ 14_js_function_parametar/function_parametar.js
@@ -0,0 +1,14 @@
+// This is a example of Function Parametar.
+function addTwoNumber (num1,num2){
+  let num3 = num1 + num2;
+  document.write(num3 + "<br>")
+};
+addTwoNumber(36,3);
+addTwoNumber(59,10);
+
+// This is another example of Function Parametar
+function myName (name){
+  document.write(name + "<br>")
+};
+myName("Rasel");
+myName("Alamin");
