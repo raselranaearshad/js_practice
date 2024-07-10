024381f6d4d90e62f6a6fed6a317e0dad93a1b37
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-11 02:58:25 +0600

    Js Function Return

---
+++ 15_js_function_return/function.js
@@ -0,0 +1,11 @@
+/* We can call function that returns a value
+    and use it in our program. */ 
+
+function myName(){
+  // This is a example of Js Function Return
+  return("Russel")
+}
+
+let addName = myName() + "'s Viper";
+
+document.write(addName);
