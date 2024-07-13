927758c3002c89e2221528afab5551b40e3a6d2b
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-13 21:48:20 +0600

    Js For Loop Over Array

---
+++ 18_js_for_loop_over_array/main.js
@@ -0,0 +1,9 @@
+// This is a example of "For Loop Over Array"
+
+let cetagory = ["Banana", "Apple", "Strawberry", "Avocado", "Mango", "Orange", "Cherry"];
+
+let i;
+
+for (i = 0; i<cetagory.length; i = i + 1){
+  document.write(cetagory[i] + "<br>")
+};
