a598e0e2d60c81fcdd6dac0c6d48e61cfb26ff4f
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-13 21:09:42 +0600

    16 Js Object

---
+++ 16_js_object/main.js
@@ -0,0 +1,21 @@
+// JavaScript Object.
+/* 
+  A JavaScript object is an entity having 
+  state & behavior (Properties & Method)
+*/
+
+// This is a example of Js Object
+let myPc = {
+  brand: "Hp",
+  screen: "15 inch",
+  isLaptop: true,
+  ram: "16GB",
+  rom: "512GB",
+  isSSD: true,
+  processor: "core i5 8th Gen"
+};
+
+ document.write(myPc['processor'])
+ /* This is another way which we can 
+    use to see the code output. */
+// document.write(myPc.brand);
