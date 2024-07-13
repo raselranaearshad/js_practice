06793f98f7ca682b77a971d0b6d5187afc51d7ad
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-13 21:28:37 +0600

    17 Js Array

---
+++ 17_js_array/main.js
@@ -0,0 +1,12 @@
+// JavaScript Array
+
+/* 
+  JavaScript array is an object that 
+  represents a connection of similar
+  type of elements.
+*/
+
+// This is a example of Js Array
+let cetagory = ["Banana", "Apple", "Strawberry", "Avocado", "Mango", "Orange", "Cherry"];
+
+document.write(cetagory[2])
