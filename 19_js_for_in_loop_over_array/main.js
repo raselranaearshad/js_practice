24f2d7a2b3b5c1bf92f5b7233f8ba8996573d225
Rasel Rana Earshad <charry.br10@gmail.com>
2024-07-16 01:27:37 +0600

    Js For In Loop Over

---
+++ 19_js_for_in_loop_over_array/main.js
@@ -0,0 +1,7 @@
+// This is a example of For In Loop Over Array
+
+let country = ["Albania", "Andorra", "Austria", "Belarus", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech", "Denmark", "European Union", "Estonia"]
+
+for (let item in country) {
+  document.write(country[item] + "<br>")
+};
